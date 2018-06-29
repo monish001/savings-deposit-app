c4022679fadb922409c4c5373f83461dabd51f57
Monish Gupta <monish.gupta1@gmail.com>
2018-06-29 08:16:28 +0530

    code auto-formatting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,8 +12,16 @@ export default class App extends React.Component {
   render() {
     const profileState = this.props.mappedProfileState;
     const showProfile = profileState.profile && profileState.profile.email;
-    const showSds = profileState.profile && profileState.profile.role && (profileState.profile.role === 'ADMIN' || profileState.profile.role === 'REGULAR_USER');
-    const showUsers = profileState.profile && profileState.profile.role && (profileState.profile.role === 'USER_MANAGER' || profileState.profile.role === 'ADMIN');
+    const showSds = // @todo handle admin and reg_user diffs.
+      profileState.profile &&
+      profileState.profile.role &&
+      (profileState.profile.role === "ADMIN" ||
+        profileState.profile.role === "REGULAR_USER");
+    const showUsers =
+      profileState.profile &&
+      profileState.profile.role &&
+      (profileState.profile.role === "USER_MANAGER" ||
+        profileState.profile.role === "ADMIN");
     return (
       <div>
         <Navbar inverse collapseOnSelect className="customNav">
@@ -25,26 +33,29 @@ export default class App extends React.Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              {showSds && <LinkContainer
-                to={{ pathname: "/saving-deposits", query: {} }}
-                onClick={() => {}}
-              >
-                <NavItem eventKey={1}>Saving Deposits</NavItem>
-              </LinkContainer>}
-              {showUsers && <LinkContainer
-                to={{ pathname: "/users", query: {} }}
-                onClick={() => {}}
-              >
-                <NavItem eventKey={2}>Users</NavItem>
-              </LinkContainer>}
-              {showProfile && <LinkContainer
-                to={{ pathname: "/profile", query: {} }}
-                onClick={() => {}}
-              >
-                <NavItem eventKey={3}>
-                  <Glyphicon glyph="user" /> Profile
-                </NavItem>
-              </LinkContainer>}
+              {showSds &&
+                <LinkContainer
+                  to={{ pathname: "/saving-deposits", query: {} }}
+                  onClick={() => {}}
+                >
+                  <NavItem eventKey={1}>Saving Deposits</NavItem>
+                </LinkContainer>}
+              {showUsers &&
+                <LinkContainer
+                  to={{ pathname: "/users", query: {} }}
+                  onClick={() => {}}
+                >
+                  <NavItem eventKey={2}>Users</NavItem>
+                </LinkContainer>}
+              {showProfile &&
+                <LinkContainer
+                  to={{ pathname: "/profile", query: {} }}
+                  onClick={() => {}}
+                >
+                  <NavItem eventKey={3}>
+                    <Glyphicon glyph="user" /> Profile
+                  </NavItem>
+                </LinkContainer>}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
