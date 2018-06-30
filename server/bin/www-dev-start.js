58712dc725842e7c615f080265f2a81a35394758
Monish Gupta <monish.gupta1@gmail.com>
2018-06-30 11:47:10 +0530

    Minor refactoring about reading env varialbes

--- server/bin/www-dev-start.js
+++ server/bin/www-dev-start.js
@@ -1,4 +1,13 @@
 require('babel-register')({
     presets: ['es2015-node6']
 });
+require('dotenv').config() // Read environment variable from .env file
+
+/**
+ * Test reading for env variables
+ */
+console.log('***', process.env.GOOGLE_SECRET);
+var config = require('config');
+console.log('***', config.authentication.google.secret);
+
 require('./www');
